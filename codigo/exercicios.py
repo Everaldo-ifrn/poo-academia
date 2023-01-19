d30c6db27597b341e89ee4eb39b6a92e6b189e5d
felijoao08 <felipe.joao1@escolar.ifrn.edu.br>
2023-01-19 00:40:46 -0300

    Update exercicios.py
    melhorei  o sentido do codigo, para cada aluno ter sua tabela em um arquivo diferente.

--- codigo/exercicios.py
+++ codigo/exercicios.py
@@ -1,24 +1,23 @@
 
 class Exercicios:
-    def __init__(self, nome, login, senha):
+    def __init__(self, nome, login):
         self.nome = nome
-        self.senha = senha
         self.login = login
-        self.path = 'C:\\Users\\joao felipe\\OneDrive\\Documents\\IFRN CURSO PROGRAMAÇA\\ACADEMIA\\'+self.nome+'.txt' #temos que  ver como podemos evitar algum erro caso ele erre o nome, ou bote diferente(mas acho que as condiçoes dao conta disso)
-
+        self.path = 'C:\\Users\\joao felipe\\OneDrive\\Documents\\IFRN CURSO PROGRAMAÇA\\ACADEMIA\\'+self.login+'.txt'
     
-    def fzrTabela(self):
-        try:    #O TRY ESTAR TRATANDO DO ERRO DE NAO EXISTIR UM ARQUIVO AINDA  E DO ERRO SE CASSO A VARIAVEL ACHEI ESTIVER VAIZA(SEM NADA).
+    def fzrTabela(self,data):
+        self.data = data
+        try:    #O TRY ESTAR TRATANDO DO ERRO DE NAO EXISTIR UM ARQUIVO AINDA  E DO ERRO SE CASSO A VARIAVEL ACHEI ESTIVER VAZIA(SEM NADA).
              with open(self.path, 'r') as arquivo:
                  tabelas = arquivo.readlines()
                  for i in tabelas:
-                     tabela = i.split('<===>')
-                     if tabela[0] == self.login and tabela[1] == self.senha+'\n':
+                     tabela = i.split('>>>>')
+                     if tabela[1] == self.data+'\n':
                         achei = 1
                         break
              
-             if achei == 1:    #o que pode evitar o erro mencionado a cima(linhas 7) é melhorar essas condiçoes 
-                 print('ops! voce ja possui uma tabela de treinos!')
+             if achei == 1:   
+                 print('ops! voce ja possui uma tabela de treinos nessa data!')
              else:
                  print('<<<<VAMOS COMEÇAR A CRIAR SEU PLANEJAMENTO>>>>')
                  print('ESTES SAO OS TREINOS: OMBRO, BICEBS, TRICiPS, PANTURRILHA, COXA, DORSAL, ABDOMEM, PEITO')
@@ -28,7 +27,7 @@ class Exercicios:
                  quinta = input('Treino da quinta: ')
                  sexta = input('Treino da sexta: ')
                  with open(self.path, 'a') as arquivo:
-                     arquivo.write(f'{self.nome}<===>{self.senha}\nsegunda: {segunda}\nTerça: {terça}\nQuarta: {quarta}\nQuinta: {quinta}\nSexta{sexta}\n')
+                     arquivo.write(f'===={self.nome}====\nData>>>>{self.data}\nsegunda: {segunda}\nTerça: {terça}\nQuarta: {quarta}\nQuinta: {quinta}\nSexta{sexta}\n')
              
         except: #BOTEI ESSE CODIGO COMO EXEÇAO PORQUE ACHEI PRATICO, JA QUE NAO FAZ SENTIDO MOSTRAR PARA O CLIENTE QUE O ARQUIVO NAO EXISTE 
              print('<<<<VAMOS COMEÇAR A CRIAR SEU PLANEJAMENTO>>>>')
@@ -39,70 +38,34 @@ class Exercicios:
              quinta = input('Treino da quinta: ')
              sexta = input('Treino da sexta: ')
              with open(self.path, 'a') as arquivo:
-                 arquivo.write(f'{self.login}<===>{self.senha}\nsegunda: {segunda}\nTerça: {terça}\nQuarta: {quarta}\nQuinta: {quinta}\nSexta: {sexta}\n')
+                 arquivo.write(f'===={self.nome}====\nData>>>>{self.data}\nsegunda: {segunda}\nTerça: {terça}\nQuarta: {quarta}\nQuinta: {quinta}\nSexta: {sexta}\n')
              print('TABELA DE TREINOS FINALIZADA')
 
-
-
-
-    def alterarTabela(self):
-         try: #aqui estar tratando do erro caso o arquivo nao exista ainda
+    def verTabelas(self):
+        try:
              with open(self.path, 'r') as arquivo:
-                 novo = arquivo.readlines()
-                 cont = 0
-                 for i in novo:
-                     cont = cont + 1
-                     tabela = i.split('<===>')
-                     if tabela[0] == self.login and tabela[1] == self.senha+'\n':
-                         for i in range(6):
-                             novo.pop(cont-1)
-                             achei = 'sim'
-                         break        
-                     else:
-                         achei = 'nao'  #aqui é para caso o login e senha nao seja encontrado
-            
-             if achei == 'sim':
-                 print('<<<<VAMOS COMEÇAR A CRIAR SEU PLANEJAMENTO>>>>')
-                 print('ESTES SAO OS TREINOS: OMBRO, BICEBS, TRICiPS, PANTURRILHA, COXA, DORSAL, ABDOMEM, PEITO')
-                 s = input('Treino da segunda: ')
-                 t = input('Treino da terça: ')
-                 q = input('Treino da quarta: ')
-                 qi = input('Treino da quinta: ')
-                 se = input('Treino da sexta: ')
-                 novo.append(f'{self.login}<===>{self.senha}\n')
-                 novo.append(f'segunda: {s}\n')
-                 novo.append(f'Terça: {t}\n')
-                 novo.append(f'Quarta: {q}\n')
-                 novo.append(f'Quinta: {qi}\n')
-                 novo.append(f'Sexta: {se}\n')
-                 with open(self.path, 'w') as arquivo:     #precisamos modificar da seguinte forma: inves de escrever tudo, escrever apenas a nova tabela
-                     ps = 0
-                     for i in novo:
-                         arquivo.write(i)
-                 print('ALTERAÇOES FINALIZADAS')
-     
-             if achei == 'nao':
-                 print('ops! nao existe tabela com esse login ou senha')
+                 tabelas = arquivo.readlines()
+                 for i in tabelas:
+                     print(i)
+        except:
+             print('Nao há nenhuma tabela registrada!')
 
-         except:
-             print('Nao existe dados!')
-    
 
 
 while True:
-    sistema = int(input('Olá, que deseja?  \n- fazer uma tabela de execicios(1), alterar a sua tabela de exercicios(2), nada(3): '))
+    sistema = int(input('Olá, que deseja?\n- fazer uma tabela de execicios(1), ver tabelas(2), nada(3): '))
     if sistema == 1:
-        y = input('Digite seu nome: ')
-        w = input('digite seu login: ')
-        x = input('Digite sua senha:')
-        pessoa = Exercicios(y, w, x)
-        pessoa.fzrTabela()
+        y = input('Digite o nome(COMPLETO): ')
+        w = input('digite o login(CPF): ')
+        z = input('Digite a data(x/x/x):') #podemos melhorar a forma que pedimos a data para evitar erros
+        pessoa = Exercicios(y, w)
+        pessoa.fzrTabela(z)
     elif sistema == 2:
-        y = input('Digite seu nome: ') 
-        w = input('digite seu login: ')
-        x = input('Digite sua senha: ')
-        pessoa = Exercicios(y, w, x)
-        pessoa.alterarTabela()
+        y = input('Digite o nome(COMPLETO): ') 
+        w = input('digite o login(CPF): ')
+        pessoa = Exercicios(y, w)
+        pessoa.verTabelas()
+
     elif sistema == 3:
         print('Até mais!')
         break
