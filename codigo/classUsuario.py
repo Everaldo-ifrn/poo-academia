2dcfa652053ff666fe48f8723a02312ad6cc5bf3
felijoao08 <felipe.joao1@escolar.ifrn.edu.br>
2023-01-20 20:56:36 -0300

    Update classUsuario.py
    fiz o menu e corrigi algumas coisas sobre o arquivo, e fiz a parte do plano ser anual ou mensal...vejo que devemos pedir o valor da mensalidade de cada plano pois, nao sao todas as academias que cobram esse valor.

--- codigo/classUsuario.py
+++ codigo/classUsuario.py
@@ -1,9 +1,9 @@
-path = 'C:\\Users\\20211174010034\\Desktop\\classUsuario.txt'
+path = 'C:\\Users\\joao felipe\\OneDrive\\Documents\\IFRN CURSO PROGRAMAÇA\\ACADEMIA\\dados pessoais(academia)\\'
 class Usuario:
     def __init__(self, cpf):          #Verificar se existe cadastro
         self.cpf = cpf
         try:
-            with open(path, 'r') as arquivo:
+            with open(path+self.cpf+'.txt', 'r') as arquivo:
                 lista = arquivo.readlines()
                 r = ''                             #r é a resposta se tá ou não cadastrado
                 for linhas in lista:
@@ -29,7 +29,7 @@ class Usuario:
         self.medicoes = str(medicoes)
         self.plano = plano
 
-        with open(path, 'w') as arquivo:
+        with open(path+self.cpf+'.txt', 'a') as arquivo:
             arquivo.write('Nome: ' + self.nome + '\n')
             arquivo.write('CPF: ' + str(self.cpf) + '\n')
             arquivo.write('Telefone: ' + self.telefone + '\n')
@@ -37,21 +37,38 @@ class Usuario:
             arquivo.write('Altura: ' + self.altura + '\n')
             arquivo.write('Peso: ' + self.peso + '\n')
             arquivo.write('Medições: ' + self.medicoes + '\n')
-            #Condições de ver o plano
-            arquivo.write('Plano: ' + self.plano + '\n')
-
+            #Aqui sera colocado no arquivo aquele plano que foi escolhido
+            if self.plano == 1:
+                     arquivo.write('PLANO>>>MENSAL''\n')
+                     for i in range(1, 13):
+                          arquivo.write(f'Més{i}: R$80\n')
+            if self.plano == 2:
+                     arquivo.write('PLANO>>>MENSAL''\n')
+                     for i in range(1, 13):
+                         arquivo.write(f'Més{i}: R$100\n')
+            
 
     def alterarDados(self, cpf):
         pass
 
 
 
+while True:
+    sistema = int(input('O que deseja fazer...\n[1] para fazer cadastro:\n[2] para alterar dados de cadastro\n[3] para ver relatorio de cadastro\n[4] para cancelar cadastro\n[5] para finalizar sistema\n>'))
+    if sistema == 1:
+        cpf = input('Por favor digit o CPF>')
+        cliente = Usuario(cpf)
+        nm = input('Digite o nome>')
+        tel = input('Digite o teelefone>')
+        end = input('Informe o endereço[Cidade, bairro, rua e numero]>')
+        alt = input('Informe a altura>')
+        ps = input('Informe o peso>')
+        mdcs = input('Informe as mediçoes(Braço, perna e cintura)>')
+        plano = int(input('informe qual o plano: ANUAL[1]/MENSAL[2]>'))
+        cliente.fazerCadastro(nm, tel, end, alt, ps, mdcs, plano)
+    if sistema == 5:
+        print('Ate mais...')
+        break
 
 
-        
 
-                         
-
-
-u1 = Usuario(202020)
-u1.fazerCadastro('Everaldo', 8491111111, 'Pedro alves, 123', 1.75, 55.0, 20, 'Mensal')
