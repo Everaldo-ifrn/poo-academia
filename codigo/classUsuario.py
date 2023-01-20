1204e8a6b535446f4dbd78acb91ecf720efc2b67
Everaldo-ifrn <99830593+Everaldo-ifrn@users.noreply.github.com>
2023-01-20 11:24:50 -0300

    Update classUsuario.py
    Refiz os códigos

--- codigo/classUsuario.py
+++ codigo/classUsuario.py
@@ -1,19 +1,57 @@
-path = 'C:\\Users\\20211174010034\\Documents\\GitHub\\classUsuario2.txt'
+path = 'C:\\Users\\20211174010034\\Desktop\\classUsuario.txt'
 class Usuario:
-    def __init__(self, nome, login, senha):
-        self.nome = nome
-        self.login = str(login)
-        self.senha = str(senha)
-
+    def __init__(self, cpf):          #Verificar se existe cadastro
+        self.cpf = cpf
         try:
             with open(path, 'r') as arquivo:
-                c = 1
-                linhas = arquivo.readlines()
-                for linha in linhas:
-                    if linha == self.login + '\n' and linhas[c+1] == self.senha + '\n': 
-                        print('achei!')
-                        c = c + 1
+                lista = arquivo.readlines()
+                r = ''                             #r é a resposta se tá ou não cadastrado
+                for linhas in lista:
+                    linha = linhas.split('\n')      #linha é do tipo string
+                    if linha[0] == str(self.cpf):
+                        r = 'sim'
+                        break
+                if r == 'sim':
+                    print('Usuário já tá cadastrado!')
+                else:
+                    print('Usuário não tá cadastrado')
+                    r = 'não'
         except:
-            print('Arquivo não existe!')                    
+            print('Usuario e arquivo não cadastrado!')
+            
+    
+    def fazerCadastro(self, nome, telefone, endereco, altura, peso, medicoes, plano):
+        self.nome = nome
+        self.telefone = str(telefone)
+        self.endereco = endereco
+        self.altura = str(altura)
+        self.peso = str(peso)
+        self.medicoes = str(medicoes)
+        self.plano = plano
 
-u1 = Usuario('everaldo', 2021, 1234)
+        with open(path, 'w') as arquivo:
+            arquivo.write('Nome: ' + self.nome + '\n')
+            arquivo.write('CPF: ' + str(self.cpf) + '\n')
+            arquivo.write('Telefone: ' + self.telefone + '\n')
+            arquivo.write('Endereço: ' + self.endereco + '\n')
+            arquivo.write('Altura: ' + self.altura + '\n')
+            arquivo.write('Peso: ' + self.peso + '\n')
+            arquivo.write('Medições: ' + self.medicoes + '\n')
+            #Condições de ver o plano
+            arquivo.write('Plano: ' + self.plano + '\n')
+
+
+    def alterarDados(self, cpf):
+        pass
+
+
+
+
+
+        
+
+                         
+
+
+u1 = Usuario(202020)
+u1.fazerCadastro('Everaldo', 8491111111, 'Pedro alves, 123', 1.75, 55.0, 20, 'Mensal')
