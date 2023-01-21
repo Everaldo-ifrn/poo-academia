64babb0710f1e88cb9160330fc7c665d23537bb2
felijoao08 <felipe.joao1@escolar.ifrn.edu.br>
2023-01-21 16:08:12 -0300

    Update classUsuario.py
    fiz algumas correçoes ,e criei o metodo relatorio.

--- codigo/classUsuario.py
+++ codigo/classUsuario.py
@@ -7,20 +7,21 @@ class Usuario:
                 lista = arquivo.readlines()
                 r = ''                             #r é a resposta se tá ou não cadastrado
                 for linhas in lista:
-                    linha = linhas.split('\n')      #linha é do tipo string
-                    if linha[0] == str(self.cpf):
+                    linha = linhas.split(':')      #linha é do tipo string, #felipe: tive que mudar o delimitador porque tava sendo escrito assim no arqv, CPF: XXXXX
+                    if linha[1] == self.cpf+'\n':
                         r = 'sim'
                         break
                 if r == 'sim':
-                    print('Usuário já tá cadastrado!')
+                     print('Usuário já tá cadastrado!')
+                     #temos que pensar como parar o codigo aqui porque se ja tiver cadastrado, nao tem sentido cadastrar de novo
                 else:
                     print('Usuário não tá cadastrado')
                     r = 'não'
         except:
-            print('Usuario e arquivo não cadastrado!')
-            
+            print('Nao há cadastro com os dados informados!')
+       
     
-    def fazerCadastro(self, nome, telefone, endereco, altura, peso, medicoes, plano):
+    def fazerCadastro(self, nome, telefone, endereco, altura, peso, medicoes, plano, mensalidade):
         self.nome = nome
         self.telefone = str(telefone)
         self.endereco = endereco
@@ -28,6 +29,7 @@ class Usuario:
         self.peso = str(peso)
         self.medicoes = str(medicoes)
         self.plano = plano
+        self.mensalidade = mensalidade
 
         with open(path+self.cpf+'.txt', 'a') as arquivo:
             arquivo.write('Nome: ' + self.nome + '\n')
@@ -41,15 +43,35 @@ class Usuario:
             if self.plano == 1:
                      arquivo.write('PLANO>>>MENSAL''\n')
                      for i in range(1, 13):
-                          arquivo.write(f'Més{i}: R$80\n')
+                          arquivo.write(f'Més{i}: R${self.mensalidade}\n')
             if self.plano == 2:
                      arquivo.write('PLANO>>>MENSAL''\n')
                      for i in range(1, 13):
-                         arquivo.write(f'Més{i}: R$100\n')
+                         arquivo.write(f'Més{i}: R${self.mensalidade}\n')
             
 
     def alterarDados(self, cpf):
         pass
+
+
+
+    def relatorio(self, cpf):
+        try:
+            with open(path+self.cpf+'.txt') as arquivo: 
+                lista = arquivo.readlines()
+                r = ''
+                for linhas in lista:
+                    linha = linhas.split(':')
+                    if linha[1] == self.cpf+'\n':
+                        r = 'sim'
+                        break
+                if r == 'sim': #se o cpf informado existir no arquivo ira mostrar o relatorio
+                    for i in lista: 
+                        print(i)
+                else:
+                    print('Dados nao encontrados!')
+        except:
+            print('Nao há cadastro com os dados informados!') #Se os dados forem errados entao nao irá abrir nenhuma arquivo
 
 
 
@@ -65,7 +87,12 @@ while True:
         ps = input('Informe o peso>')
         mdcs = input('Informe as mediçoes(Braço, perna e cintura)>')
         plano = int(input('informe qual o plano: ANUAL[1]/MENSAL[2]>'))
-        cliente.fazerCadastro(nm, tel, end, alt, ps, mdcs, plano)
+        mnsldd = float(input('Informe a mensalidade correspondente ao plano>'))
+        cliente.fazerCadastro(nm, tel, end, alt, ps, mdcs, plano, mnsldd)
+    if sistema == 3:
+        cpf = input('Por favor digit o CPF>')
+        cliente = Usuario(cpf)
+        cliente.relatorio(cpf)
     if sistema == 5:
         print('Ate mais...')
         break
