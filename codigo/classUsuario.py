e629a70a760cb3ca01e4308cefdd98b5915cc18c
felijoao08 <felipe.joao1@escolar.ifrn.edu.br>
2023-01-21 22:11:32 -0300

    Update classUsuario.py
    tentei corrigir o erro se caso ja exista o cadastro no metodo fzrcadastro.

--- codigo/classUsuario.py
+++ codigo/classUsuario.py
@@ -7,20 +7,20 @@ class Usuario:
                 lista = arquivo.readlines()
                 r = ''                             #r é a resposta se tá ou não cadastrado
                 for linhas in lista:
-                    linha = linhas.split(':')      #linha é do tipo string, #felipe: tive que mudar o delimitador porque tava sendo escrito assim no arqv, CPF: XXXXX
+                    linha = linhas.split(': ')      #linha é do tipo string, #felipe: tive que mudar o delimitador porque tava sendo escrito assim no arqv, CPF: XXXXX
                     if linha[1] == self.cpf+'\n':
                         r = 'sim'
                         break
+                    else:
+                        r == 'nao'
                 if r == 'sim':
-                     print('Usuário já tá cadastrado!')
+                     print('Usuário já cadastrado no sistema!')
+                if r == 'nao':
+                     print('Nao há cadastro com os dados informados!')
                      #temos que pensar como parar o codigo aqui porque se ja tiver cadastrado, nao tem sentido cadastrar de novo
-                else:
-                    print('Usuário não tá cadastrado')
-                    r = 'não'
-        except:
+        except: 
             print('Nao há cadastro com os dados informados!')
-       
-    
+            
     def fazerCadastro(self, nome, telefone, endereco, altura, peso, medicoes, plano, mensalidade):
         self.nome = nome
         self.telefone = str(telefone)
@@ -53,15 +53,13 @@ class Usuario:
     def alterarDados(self, cpf):
         pass
 
-
-
     def relatorio(self, cpf):
         try:
             with open(path+self.cpf+'.txt') as arquivo: 
                 lista = arquivo.readlines()
                 r = ''
                 for linhas in lista:
-                    linha = linhas.split(':')
+                    linha = linhas.split(': ')
                     if linha[1] == self.cpf+'\n':
                         r = 'sim'
                         break
@@ -71,26 +69,31 @@ class Usuario:
                 else:
                     print('Dados nao encontrados!')
         except:
-            print('Nao há cadastro com os dados informados!') #Se os dados forem errados entao nao irá abrir nenhuma arquivo
+            pass
+            #print('Nao há cadastro com os dados informados!') #Se os dados forem errados entao nao irá abrir nenhum arquivo
 
 
 
 while True:
     sistema = int(input('O que deseja fazer...\n[1] para fazer cadastro:\n[2] para alterar dados de cadastro\n[3] para ver relatorio de cadastro\n[4] para cancelar cadastro\n[5] para finalizar sistema\n>'))
     if sistema == 1:
-        cpf = input('Por favor digit o CPF>')
+        cpf = input('Por favor digite o CPF>')
         cliente = Usuario(cpf)
-        nm = input('Digite o nome>')
-        tel = input('Digite o teelefone>')
-        end = input('Informe o endereço[Cidade, bairro, rua e numero]>')
-        alt = input('Informe a altura>')
-        ps = input('Informe o peso>')
-        mdcs = input('Informe as mediçoes(Braço, perna e cintura)>')
-        plano = int(input('informe qual o plano: ANUAL[1]/MENSAL[2]>'))
-        mnsldd = float(input('Informe a mensalidade correspondente ao plano>'))
-        cliente.fazerCadastro(nm, tel, end, alt, ps, mdcs, plano, mnsldd)
+        sistema2 =int(input('Deseja continuarsim[1]/nao[2]:'))
+        if sistema2 == 1:
+             nm = input('Digite o nome>')
+             tel = input('Digite o teelefone>')
+             end = input('Informe o endereço[Cidade, bairro, rua e numero]>')
+             alt = input('Informe a altura>')
+             ps = input('Informe o peso>')
+             mdcs = input('Informe as mediçoes(Braço, perna e cintura)>')
+             plano = int(input('informe qual o plano: ANUAL[1]/MENSAL[2]>'))
+             mnsldd = float(input('Informe a mensalidade correspondente ao plano>'))
+             cliente.fazerCadastro(nm, tel, end, alt, ps, mdcs, plano, mnsldd)
+        if sistema2 == 2:
+            pass
     if sistema == 3:
-        cpf = input('Por favor digit o CPF>')
+        cpf = input('Por favor digite o CPF>')
         cliente = Usuario(cpf)
         cliente.relatorio(cpf)
     if sistema == 5:
