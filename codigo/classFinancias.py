eed12e0e2d5234ee4422b327ef36bf81f9442ba6
Everaldo-ifrn <99830593+Everaldo-ifrn@users.noreply.github.com>
2023-02-01 11:13:01 -0300

    Update classFinancias.py

--- codigo/classFinancias.py
+++ codigo/classFinancias.py
@@ -7,12 +7,12 @@ class Financias:
     def __init__(self, cpf):
         self.cpf = cpf 
         try:
-            with open(path+self.cpf+'.txt', 'r') as arquivo: #aqui estou vendo se existe cadastro
+            with open(path + self.cpf + '.txt', 'r') as arquivo: #aqui estou vendo se existe cadastro
                 linhas = arquivo.readlines()
                 r = ''
                 for dados in linhas:
                     lista = dados.split(': ')
-                    if lista[1] == self.cpf+'\n':
+                    if lista[1] == self.cpf + '\n':
                         r = 'tem'
                         break
                     else:
@@ -35,37 +35,48 @@ class Financias:
         self.mes = mes
         self.valorPago = valorPago
         try:
-            with open(path+self.cpf+'.txt', 'r') as arquivo:
+            with open(path + self.cpf + '.txt', 'r') as arquivo:
                 linhas = arquivo.readlines()
                 cont = -1
+                mesAnterior = self.mes - 1
                 for dados in linhas:
                     cont = cont + 1
-                    if dados == (f'Més-{self.mes}-R${str(self.valorPago)}\n'): #verificando se nao estar pago
+                    if dados == (f'Més-{str(mesAnterior)}-R${str(self.valorPago)}\n'):   #Verificando se o mês anterior tem pendência
+                        achei = 'pendente'
+                        break
+                    elif dados == (f'Més-{str(self.mes)}-PAGO\n'): #Verificando se estar pago
+                        achei = 'tempago'
+                        break
+                    elif dados == (f'Més-{str(self.mes)}-R${str(self.valorPago)}\n'): #verificando se nao estar pago
                         achei = 'tem'
                         break
-                    elif dados == (f'Més-{self.mes}-PAGO\n'): #Verificando se estar pago
-                        achei = 'tempago'
+                    elif 'TAXA' in linhas[8]:
+                        achei = 'taxaAnual'
                         break
                     else:
                         achei = 'ntem'
-            if achei == 'tem': #sobrecrevendo no arquivo com a fatura dada baixa
+            if achei == 'pendente':
+                print('Mes anterior com pendencia!')
+            elif achei == 'tem': #sobrecrevendo no arquivo com a fatura dada baixa
                 linhas.pop(cont)
-                linhas.insert(cont,(f'Més-{self.mes}-PAGO\n') )  
-                with open(path+self.cpf+'.txt', 'w') as arquivo:
+                linhas.insert(cont, (f'Més-{str(self.mes)}-PAGO\n'))  
+                with open(path + self.cpf + '.txt', 'w') as arquivo:
                     for i in linhas:
                         arquivo.write(i)
                     print('mensalidade dada baixa com sucesso!')
             elif achei == 'tempago':
                 print('Mesnsalidade já tá paga!')
             elif achei == 'ntem':
-                print('Dados informados errados...ou taxa pendente!')
+                print('Dados informados errados...')
+            elif achei == 'taxaAnual':
+                print('Há uma taxa de contrato anual!')
         except:
             print('Erro...')
     
     
     def darBaixaTaxa(self, valorPago):
         self.valorPago = valorPago
-        with open(path+self.cpf+".txt", 'r') as arquivo:
+        with open(path + self.cpf + ".txt", 'r') as arquivo:
             linhas = arquivo.readlines()
             achei = ''
             if linhas[8] == (f'TAXA: R${valorPago}\n'): #aqui achei melhor verificar na pspsiçao que a gente definiu que a taxa estaria
