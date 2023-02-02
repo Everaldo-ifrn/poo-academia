2736d6080d7413e6adb1d918f263e0b3a8325bc8
Everaldo-ifrn <99830593+Everaldo-ifrn@users.noreply.github.com>
2023-02-02 10:21:09 -0300

    Update classExercicios.py

--- codigo/classExercicios.py
+++ codigo/classExercicios.py
@@ -20,6 +20,36 @@ class TabelaExercicios:
         except:
             print('Opá, tabela não cadastrada!')  
     
-    def fazerTabela(self, codigo, diaSemana, treino):
-        pass
-                        
+    def fazerTabela(self, diaSemana, treino):
+        print('teste')
+        self.diaSemana = diaSemana
+        self.treino = treino
+        tabelas = [[],[],[],[],[],[]]
+        while True:
+            if self.diaSemana == 1:
+                tabelas.insert(0, f'SEGUNDA:{self.treino}\n')
+                
+            elif self.diaSemana == 2:
+                tabelas.insert(1, f'TERÇA:{self.treino}\n')
+                
+            elif self.diaSemana == 3:
+                tabelas.insert(2, f'QUARTA:{self.treino}\n')
+                
+            elif self.diaSemana == 4:
+                tabelas.insert(3, f'QUINTA:{self.treino}\n')
+            
+            elif self.diaSemana == 5:
+                tabelas.insert(4, f'SEXTA:{self.treino}\n')
+                
+            elif self.diaSemana == 6:
+                tabelas.insert(5, f'SABADO:{self.treino}\n')
+                
+            elif self.diaSemana == 0:
+                with open(path + str(self.codigo) + '.txt', 'a') as arquivo:
+                    for i in tabelas:
+                        if i != []:
+                            arquivo.write(i)
+                break
+
+        print(tabelas)
+                                
