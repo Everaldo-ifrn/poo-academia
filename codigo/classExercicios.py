76fced89b53a5021b513d274b718b2657354f6ed
felijoao08 <felipe.joao1@escolar.ifrn.edu.br>
2023-02-02 19:15:49 -0300

    Update classExercicios.py

--- codigo/classExercicios.py
+++ codigo/classExercicios.py
@@ -1,5 +1,5 @@
 from classUsuario import Usuario
-path = 'C:\\Users\\20211174010034\\Desktop\\'
+path = 'C:\\Users\\joao felipe\\OneDrive\\Área de Trabalho\\dados de poo academia\\TABELAS\\'
 
 #Usuario.cpf
 class TabelaExercicios:
@@ -21,35 +21,36 @@ class TabelaExercicios:
             print('Opá, tabela não cadastrada!')  
     
     def fazerTabela(self, diaSemana, treino):
-        print('teste')
         self.diaSemana = diaSemana
         self.treino = treino
         tabelas = [[],[],[],[],[],[]]
         while True:
             if self.diaSemana == 1:
                 tabelas.insert(0, f'SEGUNDA:{self.treino}\n')
-                
+                break
+            
             elif self.diaSemana == 2:
                 tabelas.insert(1, f'TERÇA:{self.treino}\n')
+                break
                 
             elif self.diaSemana == 3:
                 tabelas.insert(2, f'QUARTA:{self.treino}\n')
+                break
                 
             elif self.diaSemana == 4:
                 tabelas.insert(3, f'QUINTA:{self.treino}\n')
+                break
             
             elif self.diaSemana == 5:
                 tabelas.insert(4, f'SEXTA:{self.treino}\n')
+                break
                 
             elif self.diaSemana == 6:
                 tabelas.insert(5, f'SABADO:{self.treino}\n')
-                
-            elif self.diaSemana == 0:
+                break
+            elif diaSemana == 0:
                 with open(path + str(self.codigo) + '.txt', 'a') as arquivo:
                     for i in tabelas:
                         if i != []:
                             arquivo.write(i)
                 break
-
-        print(tabelas)
-                                
