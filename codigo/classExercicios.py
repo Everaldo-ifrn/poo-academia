5ccffa785a992d4d7db74485800f981b39ef3fec
felijoao08 <felipe.joao1@escolar.ifrn.edu.br>
2023-02-03 01:23:32 -0300

    Update classExercicios.py

--- codigo/classExercicios.py
+++ codigo/classExercicios.py
@@ -1,5 +1,5 @@
 from classUsuario import Usuario
-path = 'C:\\Users\\Everaldo Junior\\Desktop\\'
+path = 'C:\\Users\\joao felipe\\OneDrive\\Área de Trabalho\\dados de poo academia\\TABELAS\\'
 
 #Usuario.cpf
 class TabelaExercicios:
@@ -20,57 +20,40 @@ class TabelaExercicios:
         except:
             print('Opá, tabela não cadastrada!')  
     
-    def fazerTabela(self, diaSemana, treino):
+    def fazerTabela(self, diaSemana, treino, objetivo):
         self.diaSemana = diaSemana
         self.treino = treino
-        tabelas = [[],[],[],[],[],[]]
-        while True:
-            if self.diaSemana == 1:
-                tabelas.insert(0, f'SEGUNDA:{self.treino}\n')
-                break
+        self.objetivo = objetivo
+        tabelas = [[],[],[],[],[],[],[]]
+        tabelas[0] = (f'CODIGO: {self.codigo}\n')
+        tabelas[1] = (f'OBJETIVO: {self.objetivo}\n')
+        print(tabelas)
+    
             
-            elif self.diaSemana == 2:
-                tabelas.insert(1, f'TERÇA:{self.treino}\n')
-                break
-                
-            elif self.diaSemana == 3:
-                tabelas.insert(2, f'QUARTA:{self.treino}\n')
-                break
-                
-            elif self.diaSemana == 4:
-                tabelas.insert(3, f'QUINTA:{self.treino}\n')
-                break
+        if self.diaSemana == 1:
+            tabelas.insert(2, (f'SEGUNDA:{self.treino}\n'))
+              
             
-            elif self.diaSemana == 5:
-                tabelas.insert(4, f'SEXTA:{self.treino}\n')
-                break
+        elif self.diaSemana == 2:
+            tabelas.insert(3, (f'TERÇA:{self.treino}\n'))
+               
                 
-            elif self.diaSemana == 6:
-                tabelas.insert(5, f'SABADO:{self.treino}\n')
-                break
-            elif diaSemana == 0:
-                ##############################################
-                #with open(path + str(self.codigo) + '.txt', 'r') as arquivo:
-                #    linhas = arquivo.readlines()
-
-                #with open(path + str(self.codigo) + '.txt', 'a') as arquivo:
-                #    arquivo.write('1234\n')
-                #    pass
+        elif self.diaSemana == 3:
+            tabelas.insert(4, (f'QUARTA:{self.treino}\n'))
+               
                 
-                """with open(path + str(self.codigo) + '.txt', 'w') as arquivo:     #Reescrevendo...
-                    arquivo.write(self.cpf + '\n')
-                    #arquivo.write(str(self.codigo) + '\n')
-                    for linha in linhas:
-                        arquivo.write(linha)"""
-                ##############################################
-
-                break
-#Falta colocar o codigo no começo de cada semana de treino!
-
-        with open(path + str(self.codigo) + '.txt', 'a') as arquivo:
-            #arquivo.write('1234\n') ##################
-            for i in tabelas:
-                if i != []:
-                    arquivo.write(i)
-            #linhas = arquivo.readlines()
-            #print(linhas)
+        elif self.diaSemana == 4:
+            tabelas.insert(5, (f'QUINTA:{self.treino}\n'))
+                
+            
+        elif self.diaSemana == 5:
+            tabelas.insert(6, (f'SEXTA:{self.treino}\n'))
+            
+        elif diaSemana == 0:
+            with open(path + str(self.codigo) + '.txt', 'a') as arquivo:
+                for i in tabelas:
+                    if i != []:
+                        arquivo.write(i)
+        
+                        
+        #O BuUG QUE ESTAR OCORRENDO NAOOOO TEM LOGICA NENHUMA!!! 1:20 JA E EU N DESENROLEI, VOU DORMIR
