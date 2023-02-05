6ef98686886d3356de09b5ffb55c2235ddd65a01
Everaldo-ifrn <everaldo.j@escolar.ifrn.edu.br>
2023-02-05 11:59:13 -0300

    Update classExercicios.py

--- codigo/classExercicios.py
+++ codigo/classExercicios.py
@@ -3,28 +3,34 @@ import path
 
 
 class TabelaExercicios:
-    def __init__(self, codigo, cpf):
+    def __init__(self, codigo, cpf, c):
         self.codigo = codigo
         self.cpf = cpf
-        self.c = 1  #nº de cada semana de treino criada
+        self.c = c  #nº de cada semana de treino criada
 
         try:
             with open(path.pathT + str(self.codigo) + '.txt', 'r') as arquivo:
                 linhas = arquivo.readlines()
-                linhas = linhas.split('\n')
 
                 for linha in linhas:
-                    if cpf == linha[0]:
-                        print('Tabela já cadastrada!')
+                    linha = linha.split(': ')
+                    if str(self.cpf) + '\n' == linha[1]:
+                        achei = 'sim'
+                        break
                     else:
-                        print('Tabela não cadastrada!')
+                        achei = 'não'
+
+                if achei == 'sim':
+                    print('>> Tabela já cadastrada! <<')
+                else:
+                    print('>> Tabela não cadastrada! <<')
+
         except:
-            print('Opá, tabela não cadastrada!')  
+           print('>> Opá, tabela não cadastrada! <<')  
     
-    def fazerTabela(self, semanaTreino, objetivo, c):
+    def fazerTabela(self, semanaTreino, objetivo):
         self.semanaTreino = semanaTreino
         self.objetivo = objetivo
-        self.c = c
 
         try:
             with open(path.pathT + str(self.codigo) + '.txt', 'r') as arquivo:
@@ -33,19 +39,65 @@ class TabelaExercicios:
             with open(path.pathT + str(self.codigo) + '.txt', 'w') as arquivo:
                 for linha in linhas:
                     arquivo.write(linha)
-                arquivo.write(self.objetivo + '\n')
-                arquivo.write('Semana ' + str(self.c) + '\n')
+                arquivo.write('Semana: ' + str(self.c) + '\n')
+                arquivo.write('Objetivo: ' + self.objetivo + '\n')
                 for dia in semanaTreino:
                     arquivo.write(dia + '\n')
                 
 
-
         except:                      #Quando for a 1ª lista criada
             with open(path.pathT + str(self.codigo) + '.txt', 'w') as arquivo:
-                arquivo.write(self.cpf + '\n')
-                arquivo.write(self.objetivo + '\n')
-                arquivo.write('Semana ' + str(self.c) + '\n')
+                arquivo.write('CPF: '+ self.cpf + '\n')
+                arquivo.write('Semana: ' + str(self.c) + '\n')
+                arquivo.write('Objetivo: ' + self.objetivo + '\n')
                 for dia in semanaTreino:
                     arquivo.write(dia + '\n')
         self.c = self.c + 1     #nº das semanas
-#Resolva o problema do self.c está sendo = 1 em todas as semanas!!!
+
+        print('Tabela de exercício criada!')
+
+    def verTabela(self):
+        try:
+            with open(path.pathT + str(self.codigo) + '.txt', 'r') as arquivo:
+                linhas = arquivo.readlines()
+                for linha in linhas:
+                    print(linha)
+        except:
+            print('>> Tabela de exercício não existe! <<')
+
+
+    def alterarTabela(self, tabelaAlterar, semanaTreinoNova, objetivo):
+        self.tabelaAlterar = tabelaAlterar  #nº da semana a ser mudada
+        self.semanaTreinoNova = semanaTreinoNova     #nova semana 
+        self.objetivo = objetivo 
+        try:
+            with open(path.pathT + str(self.codigo) + '.txt', 'r') as arquivo:
+                linhas = arquivo.readlines()
+                i = 0       #índice da linha da semana que será apagada
+                for linha in linhas:
+                    linha = linha.split(': ')
+                    if linha[1] == str(self.tabelaAlterar) + '\n':
+                        break
+                    i = i + 1
+
+                
+                try:
+                    while linhas[i] != ('Semana: ' + str(self.tabelaAlterar + 1) + '\n'):
+                        linhas.pop(i)
+                except IndexError:
+                    pass
+
+                cont = 1
+                for dia in semanaTreinoNova:
+                    linhas.insert(i, semanaTreinoNova[- cont] + '\n')
+                    cont = cont + 1
+                linhas.insert(i, 'Objetivo: ' + str(self.objetivo) + '\n')
+                linhas.insert(i, 'Semana: ' + str(self.tabelaAlterar) + '\n')
+                    
+            with open(path.pathT + str(self.codigo) + '.txt', 'w') as arquivo:
+                for linha in linhas:
+                    arquivo.write(linha)
+            print('>> Tabela Alterada com sucesso! <<')
+
+        except:
+            print('>> Tabela de exercício não existe! <<')
