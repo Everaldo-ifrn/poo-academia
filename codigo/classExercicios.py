9e55e5859ef3c75d4de3e4a22849636966f13d40
Everaldo-ifrn <everaldo.j@escolar.ifrn.edu.br>
2023-02-04 23:11:52 -0300

    Update classExercicios.py

--- codigo/classExercicios.py
+++ codigo/classExercicios.py
@@ -6,6 +6,7 @@ class TabelaExercicios:
     def __init__(self, codigo, cpf):
         self.codigo = codigo
         self.cpf = cpf
+        self.c = 1  #nº de cada semana de treino criada
 
         try:
             with open(path.pathT + str(self.codigo) + '.txt', 'r') as arquivo:
@@ -20,51 +21,31 @@ class TabelaExercicios:
         except:
             print('Opá, tabela não cadastrada!')  
     
-    def fazerTabela(self, diaSemana, treino, objetivo):
-        self.diaSemana = diaSemana
-        self.treino = treino
+    def fazerTabela(self, semanaTreino, objetivo, c):
+        self.semanaTreino = semanaTreino
         self.objetivo = objetivo
-        tabelas = [[],[],[],[],[],[],[]]
-        while True:               #Coloquie de novo o while para acabar com o erro de escrever codigo e objetivo no final
-            if diaSemana == 0:        #a cada dia ele está escrevendo codigo e objetivo e não a cada semana!
-                break
+        self.c = c
 
-            tabelas[0] = (f'CODIGO: {self.codigo}\n')
-            tabelas[1] = (f'OBJETIVO: {self.objetivo}\n')
-        
-                
-            if self.diaSemana == 1:
-                tabelas.insert(2, (f'SEGUNDA:{self.treino}\n'))
-                
-                
-            elif self.diaSemana == 2:
-                tabelas.insert(3, (f'TERÇA:{self.treino}\n'))
-                
-                    
-            elif self.diaSemana == 3:
-                tabelas.insert(4, (f'QUARTA:{self.treino}\n'))
-                
-                    
-            elif self.diaSemana == 4:
-                tabelas.insert(5, (f'QUINTA:{self.treino}\n'))
-                    
-                
-            elif self.diaSemana == 5:
-                tabelas.insert(6, (f'SEXTA:{self.treino}\n'))
-
-
-        
-
-            print(tabelas)
-            with open(path.pathT + str(self.codigo) + '.txt', 'r') as arquivo:          #lendo o que já existe
-                    linhas = arquivo.readlines()
+        try:
+            with open(path.pathT + str(self.codigo) + '.txt', 'r') as arquivo:
+                linhas = arquivo.readlines()
             
-
-            with open(path.pathT + str(self.codigo) + '.txt', 'w') as arquivo:          #reescrevendo o que já existe
+            with open(path.pathT + str(self.codigo) + '.txt', 'w') as arquivo:
                 for linha in linhas:
-                    arquivo.write(linha)  
+                    arquivo.write(linha)
+                arquivo.write(self.objetivo + '\n')
+                arquivo.write('Semana ' + str(self.c) + '\n')
+                for dia in semanaTreino:
+                    arquivo.write(dia + '\n')
+                
 
-                for i in tabelas:                                                 #escrevendo com novo
-                    if i != []:
-                        arquivo.write(i)
-            break
+
+        except:                      #Quando for a 1ª lista criada
+            with open(path.pathT + str(self.codigo) + '.txt', 'w') as arquivo:
+                arquivo.write(self.cpf + '\n')
+                arquivo.write(self.objetivo + '\n')
+                arquivo.write('Semana ' + str(self.c) + '\n')
+                for dia in semanaTreino:
+                    arquivo.write(dia + '\n')
+        self.c = self.c + 1     #nº das semanas
+#Resolva o problema do self.c está sendo = 1 em todas as semanas!!!
