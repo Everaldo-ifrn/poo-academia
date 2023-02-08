09e61736be8fa28cfbf5d6585e87bd0ee2b18ce7
Everaldo-ifrn <everaldo.j@escolar.ifrn.edu.br>
2023-02-07 23:17:54 -0300

    Update main.py

--- codigo/main.py
+++ codigo/main.py
@@ -9,13 +9,13 @@ while True:
      cliente = Usuario(cpf)
      print(' ')
      try:
-        sistemaPrincipal = int(input('Para que area do sistema deseja se dirigir...CADASTROS[1], FINANCIAS[2], TABELAS[3]>>>')) 
+        sistemaPrincipal = int(input('\nPara que area do sistema deseja se dirigir: |1| CADASTROS \n|2| FINANCIAS \n|3| TABELAS \n> ')) 
         if sistemaPrincipal == 1:
-            sistema = int(input('O que deseja fazer...\n[1] para fazer cadastro:\n[2] para alterar dados de cadastro\n[3] para ver relatorio de cadastro\n[4] para cancelar cadastro\n[5] para finalizar sistema\n>'))
+            sistema = int(input('\nO que deseja fazer...\n[1] para fazer cadastro:\n[2] para alterar dados de cadastro\n[3] para ver relatorio de cadastro\n[4] para cancelar cadastro\n[5] para finalizar sistema\n>'))
             print(' ')
 
             if sistema == 1:
-                nm = input('Digite o nome> ')
+                nm = input('\nDigite o nome> ')
                 tel = input('Digite o telefone> ')
                 end = input('Informe o endereço[Cidade, bairro, rua e numero]> ')
                 alt = input('Informe a altura> ')
@@ -27,47 +27,47 @@ while True:
                 print(' ')
 
             elif sistema == 2:
-                cpf = input('Informe o CPF correspondente ao cadastro que deseja mudar os dados...> ')
+                cpf = input('\nInforme o CPF correspondente ao cadastro que deseja mudar os dados...> ')
                 cliente.alterarDados(cpf)
                 print(' ')
 
             elif sistema == 3:
-                cpf = input('Informe o CPF correspondente ao cadastro que deseja olhar o relatorio...> ')
+                cpf = input('\nInforme o CPF correspondente ao cadastro que deseja olhar o relatorio...> ')
                 cliente.relatorio(cpf)
                 print(' ')
 
             elif sistema  == 4:
-                cpf = input('Informe o CPF correspondente ao cadastro que deseja cancelar...> ')
+                cpf = input('\nInforme o CPF correspondente ao cadastro que deseja cancelar...> ')
                 cliente.cancelarCadastro(cpf)
                 print(' ')
 
             elif sistema == 5:
-                print('Ate mais...')
+                print('\nAte mais...')
                 break
             else:
-                print('>> Você digitou algo fora da lista dada, tente novamente! <<')
+                print('\n>> Você digitou algo fora da lista dada, tente novamente! << \n')
         elif sistemaPrincipal == 2:
                 
             clienteF = Financias(cliente.cpf)
-            sistema = int(input('O que deseja fazer... \n[1] Dar baixa na fatura\n[2] Dar baixa em uma taxa\n>'))
+            sistema = int(input('\nO que deseja fazer... \n|1| Dar baixa na fatura\n|2| Dar baixa em uma taxa\n>'))
                 
             if sistema == 1:
-                mes = int(input('digite o numero do mes 1, 2, 3...>'))
-                valor = float(input('Informe o valor da mensalidade>'))
+                mes = int(input('\nDigite o numero do mes 1, 2, 3... \n> '))
+                valor = float(input('\nInforme o valor da mensalidade \n> '))
                 clienteF.darBaixaFatura(mes, valor)
 
             elif sistema == 2:
-                valorT = float(input('Informe o valor da taxa>'))
+                valorT = float(input('\nInforme o valor da taxa \n> '))
                 clienteF.darBaixaTaxa(valorT)
         
 
         elif sistemaPrincipal == 3: #TABELAS
-            sistema = int(input('O que deseja fazer... \n|1| Fazer tabela de exercício \n|2| Alterar tabela de exercício \n|3| Ver tabela de exercício \n'))
+            sistema = int(input('\nO que deseja fazer... \n|1| Fazer tabela de exercício \n|2| Alterar tabela de exercício \n|3| Ver tabela de exercício \n'))
             #codigo = int(input('\nDigite código da conta> '))
                          
 
             if sistema == 1:
-                r = int(input('Qual objetivo do treino? \n|1| Esmagrecer \n|2| Ganhar massa muscular \n|3| Definir os muscuslos \n> '))
+                r = int(input('\nQual objetivo do treino? \n|1| Esmagrecer \n|2| Ganhar massa muscular \n|3| Definir os muscuslos \n> '))
                 if r == 1:
                     objetivo = 'Esmagrecer'
                 elif r == 2:
@@ -81,7 +81,7 @@ while True:
                 semana = ['Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta']    #Dias que a academia "Normal" funciona
                 semanaTreino = []               #Lista dos dias que o usuário vai treinar
                 for dia in semana:          #criei a lista só com os dias que ele vai treinar
-                    re = int(input(f'\nVocê vai treinar na {dia}? \n|1| Sim \n|2| Não \n> '))
+                    re = int(input(f'\nVai treinar na {dia}? \n|1| Sim \n|2| Não \n> '))
                     if re == 1:
                         treino = input('\nDigite o treino e a repetição desse dia, (exemplo: Perna, 15) \n> ')
                         semanaTreino.append(dia +': '+ treino)    #coloco o treino na mesma lista dos dias
@@ -92,7 +92,7 @@ while True:
             elif sistema == 2:
                 tabelaAlterar = int(input('\nQual tabela você quer alterar? (Exemplo: Semana 1, digite (1)) \n> '))
                 clienteE = TabelaExercicios(cliente.cpf, c)                
-                r = int(input('Qual objetivo do treino? \n|1| Esmagrecer \n|2| Ganhar massa muscular \n|3| Definir os muscuslos \n> '))
+                r = int(input('\nQual objetivo do treino? \n|1| Esmagrecer \n|2| Ganhar massa muscular \n|3| Definir os muscuslos \n> '))
                 if r == 1:
                     objetivo = 'Esmagrecer'
                 elif r == 2:
@@ -113,8 +113,9 @@ while True:
 
 
             elif sistema == 3:
+                clienteE = TabelaExercicios(cliente.cpf, c)
                 clienteE.verTabela() 
                 
      except ValueError:
-         print('>> Você digitou algo fora da lista dada, tente novamente! <<')
+         print('\n>> Você digitou algo fora da lista dada, tente novamente! <<\n')
 #ERRO: Quando altero a semana de treino e crio outra, o numero de semanas tipo "rezeta", não continua a ordem!
